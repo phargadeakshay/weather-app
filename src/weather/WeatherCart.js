6e17bab909ff57477a2174875e639568347c85d6
Akshay Phargade <phargadeakshay@gmail.com>
2023-02-06 00:39:25 +0530

    refresh page

--- src/weather/WeatherCart.js
+++ src/weather/WeatherCart.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState, useMemo } from "react";
 import sunset from "../png/sunsetnew.png";
+import cloudy from "../png/cloudy1.png";
+// import sunset2 from "../png/sunset2.png";
+import sunset2 from "../png/sunset6.jpg";
+
 import sunrise from "../png/sunrise.png";
+import sunrise3 from "../png/sunrise3.jpg";
 import cities from "./Cities.json";
 import { fetchweatherdata } from "../Slices/WeatherSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { STATUSES } from "../Slices/WeatherSlice";
 import Loading from "react-loading";
+
 
 const WeatherCart = () => {
   const [weatheData, setweatheData] = useState([]);
@@ -14,8 +20,8 @@ const WeatherCart = () => {
   const [Lon, setLon] = useState();
   const dispatch = useDispatch();
   const [LatandLon, setLatandLon] = useState([
-    { lat: 24.833333, lon: 87.8 },
-    { lat: 20.9, lon: 74.783333 },
+    // { lat: 24.833333, lon: 87.8 },
+    // { lat: 20.9, lon: 74.783333 },
   ]);
   let index = 0;
 
@@ -69,31 +75,17 @@ const WeatherCart = () => {
 
 
 
-//   useEffect(() => {
-//     const intervalId = setInterval(() => {
-//       if (index === LatandLon.length) {
-//         index = 0;
-//       }
-//       const { lat, lon } = LatandLon[index];
-//       dispatch(fetchweatherdata(LatandLon[index]))
-//           index++;
-      
-//     }, 3000);
-//     return () => clearInterval(intervalId);
-//   }, [LatandLon]);
-
-
 // useEffect(() => {
-//     const intervalId = setInterval(() => {
-//         setweatheData([])
+//   const intervalId = setInterval(() => {
+//       setweatheData([]); // reset the weather data here
 //       LatandLon.forEach(({ lat, lon }) => {
 //         const obj ={lat:lat,lon:lon}
 //         dispatch(fetchweatherdata(obj))
 //       });
-//     }, 10000);
-//     // setweatheData([])
-//     // return () => clearInterval(intervalId);
+//   }, 10000);
+//   return () => clearInterval(intervalId);
 // }, [LatandLon]);
+
 
 const DeleteCity=(id)=>{
 let filt = weatheData.filter((item)=>{
@@ -104,22 +96,9 @@ let filt = weatheData.filter((item)=>{
 setweatheData(filt)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
   if (status === STATUSES.LOADING) {
     return (
       <div className="flex justify-center">
-        {" "}
         <Loading type="bubbles" color="#ff0000" height={667} width={375} />
       </div>
     );
@@ -144,36 +123,36 @@ setweatheData(filt)
             </button>
         </div>
       </header>
-<hr className="text-yellow-400 border-2"/>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:gap-20 p-3  bg-white">
+<hr className="text-yellow-400 border-2 "/>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:gap-10 p-3  bg-white">
         {weatheData &&
           weatheData.map((item) => {
             return (
-              <div className="w-full border bg-gray-200 p-5 rounded-lg">
+              <div className="w-full border border-black bg-yellow-100 p-5 rounded-lg h-[380px]">
                 <div className="flex justify-between items-center">
                   <p className="text-xl font-bold text-gray-500">{item.name}</p>
                   <i className="far fa-trash-alt add-btn text-red-600 text-2xl" onClick={()=>{DeleteCity(item.id)}}></i>
                 </div>
-                <hr className=" border-1 mt-2 border-yellow-400" />
+                <hr className=" border-1 mt-2 border-red-600" />
                 <div className="w-full mt-2 flex justify-end">
-                  <img src={sunset} alt="" className="w-10 h-10" />
+                  <img src={cloudy} alt="" className="w-[70px] h-[70px] rounded-md" />
                 </div>
                 <div className="rounded overflow-hidden  flex justify-center flex-col items-center">
-                  <span alt="ecommerce" className="text-yellow-600 text-9xl">
+                  <span alt="ecommerce" className="text-yellow-600 text-8xl font-semibold">
                     {item.temp.toFixed(0)}
                     <sup>'</sup>C
                   </span>
-                  <p className="font-bold text-gray-500">{item.feels_like}</p>
+                  <p className="font-bold text-gray-500">Feels like:{item.feels_like}</p>
                 </div>
                 <div className="flex justify-around mt-3 w-full">
                   <div className="flex flex-col items-center ">
-                    <img src={sunrise} alt="" className="w-12 h-15" />
+                    <img src={sunrise3} alt="" className="w-13 rounded-md h-12" />
                     <span className="font-bold text-gray-500">
                       {new Date(item.sunrise * 1000).toLocaleTimeString()}
                     </span>
                   </div>
                   <div className="flex flex-col items-center">
-                    <img src={sunset} alt="" className="w-12 h-12" />
+                    <img src={sunset2} alt="" className="w-13 rounded-md h-12" />
                     <span className="font-bold text-gray-500">
                       {convertTime(item.sunset)}
                     </span>
